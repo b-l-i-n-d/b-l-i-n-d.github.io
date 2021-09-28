268d2b6d2edd99476eca3c08fbb3ad210473e10f
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-09-28 17:03:12 +0600

    Fixed some issue with typed.js
    Also added links of my project

--- scripts/script.js
+++ scripts/script.js
@@ -17,10 +17,11 @@ var typed = new Typed("#typing", {
     strings: ["web application developer", "software engineer", "full stack engineer"],
     typeSpeed: 100,
     backSpeed: 60,
+    smartBackspace: true,
     loop: true
 });
-
-var typed = new Typed("#typing-2", {
+    
+var typed2 = new Typed("#typing-2", {
     strings: ["web application developer", "software engineer", "full stack engineer"],
     typeSpeed: 100,
     backSpeed: 60,
