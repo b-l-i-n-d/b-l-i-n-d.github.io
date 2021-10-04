983bf5b7b82e3ee333e55f33d4fda39170279c45
Fahim Faisal <42368238+b-l-i-n-d@users.noreply.github.com>
2021-10-04 15:14:22 +0600

    Home section added

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typing", {
+    strings: ["web application developer", "software engineer", "full stack engineer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    smartBackspace: true,
+    loop: true
+});
