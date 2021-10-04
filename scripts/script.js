37fb1807f87c769625377a65e0238b3803ceb268
Fahim Faisal <42368238+b-l-i-n-d@users.noreply.github.com>
2021-10-04 16:30:59 +0600

    About section

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,11 @@ var typed = new Typed("#typing", {
     smartBackspace: true,
     loop: true
 });
+
+var typed2 = new Typed("#typing-2", {
+    strings: ["web application developer", "software engineer", "full stack engineer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    smartBackspace: true,
+    loop: true
+});
