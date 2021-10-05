ddf0efc2bae3aac1230caf1279b9d8bb0646f89c
Fahim Faisal <42368238+b-l-i-n-d@users.noreply.github.com>
2021-10-05 18:04:16 +0600

    Contact and footer section added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+});
+
 var typed = new Typed("#typing", {
     strings: ["web application developer", "software engineer", "full stack engineer"],
     typeSpeed: 100,
