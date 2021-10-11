908f2e6fb6741fcbbd22a0c3a5fb46e6c6ae8184
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-10-11 13:23:48 +0600

    Home section added

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -4,3 +4,11 @@ $(document).ready(function() {
         $('.menu-btn i').toggleClass("active");
     })
 });
+
+var typed = new Typed("#typing", {
+    strings: ["web application developer", "software engineer", "full stack engineer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    smartBackspace: true,
+    loop: true
+});
