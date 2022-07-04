6d014d88a98256118ed3e4eb14630a124b768d1d
Fahim Faisal <fahim.faisal.abir@gmail.com>
2022-07-04 23:05:19 +0600

    Added current active nav menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { BsChevronDown, BsLinkedin } from 'react-icons/bs';
 import { FaFacebook, FaGithub } from 'react-icons/fa';
 import { FiMail } from 'react-icons/fi';
@@ -9,10 +9,57 @@ import { HiMenuAlt1, HiMenuAlt3 } from 'react-icons/hi';
 import { themeChange } from 'theme-change';
 import Theme from './Theme';
 
+const clamp = (value) => Math.max(0, value);
+
+const isBetween = (value, floor, ceil) => value >= floor && value <= ceil;
+
+const useScrollspy = (ids, offset = 0) => {
+    const [activeId, setActiveId] = useState('');
+
+    useLayoutEffect(() => {
+        const listener = () => {
+            const scroll = window.pageYOffset;
+
+            const position = ids
+                .map((id) => {
+                    const element = document.getElementById(id);
+
+                    if (!element) return { id, top: -1, bottom: -1 };
+
+                    const rect = element.getBoundingClientRect();
+                    const top = clamp(rect.top + scroll - offset);
+                    const bottom = clamp(rect.bottom + scroll - offset);
+
+                    return { id, top, bottom };
+                })
+                .find(({ top, bottom }) => isBetween(scroll, top, bottom));
+
+            setActiveId(position?.id || '');
+        };
+
+        listener();
+
+        window.addEventListener('resize', listener);
+        window.addEventListener('scroll', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener);
+            window.removeEventListener('scroll', listener);
+        };
+    }, [ids, offset]);
+
+    return activeId;
+};
+
 function Navbar() {
     useEffect(() => {
         themeChange(false);
     });
+
+    const activeId = useScrollspy(
+        ['header', 'about', 'skills', 'education', 'experiences', 'projects', 'contact'],
+        81
+    );
 
     return (
         <div className="sticky top-0 z-50 border-b border-base-300 bg-opacity-30 shadow-none backdrop-blur-lg backdrop-filter">
@@ -21,19 +68,25 @@ function Navbar() {
                     <div className="hidden space-x-5 lg:flex">
                         <a
                             href="#header"
-                            className="rounded border-primary transition-all duration-150 hover:border-b-4 hover:text-primary active:border-dotted"
+                            className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary hover:text-primary ${
+                                activeId === 'header' && 'border-b-4 border-solid border-primary'
+                            }`}
                         >
                             Home
                         </a>
                         <a
                             href="#about"
-                            className="rounded border-primary transition-all duration-150 hover:border-b-4 hover:text-primary active:border-dotted"
+                            className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary hover:text-primary ${
+                                activeId === 'about' && 'border-b-4 border-solid border-primary'
+                            }`}
                         >
                             About
                         </a>
                         <a
                             href="#contact"
-                            className="rounded border-primary transition-all duration-150 hover:border-b-4 hover:text-primary active:border-dotted"
+                            className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary hover:text-primary ${
+                                activeId === 'contact' && 'border-b-4 border-solid border-primary'
+                            }`}
                         >
                             Contact
                         </a>
@@ -46,16 +99,48 @@ function Navbar() {
                                 className="no-scrollbar dropdown-content menu rounded-box mt-4 w-auto bg-base-100 p-2 shadow"
                             >
                                 <li>
-                                    <a href="#skills">Skills</a>
+                                    <a
+                                        href="#skills"
+                                        className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                            activeId === 'skills' &&
+                                            'border-b-4 border-solid border-primary'
+                                        }`}
+                                    >
+                                        Skills
+                                    </a>
                                 </li>
                                 <li>
-                                    <a href="#experiences">Experiences</a>
+                                    <a
+                                        href="#experiences"
+                                        className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                            activeId === 'experiences' &&
+                                            'border-b-4 border-solid border-primary'
+                                        }`}
+                                    >
+                                        Experiences
+                                    </a>
                                 </li>
                                 <li>
-                                    <a href="#education">Education</a>
+                                    <a
+                                        href="#education"
+                                        className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                            activeId === 'education' &&
+                                            'border-b-4 border-solid border-primary'
+                                        }`}
+                                    >
+                                        Education
+                                    </a>
                                 </li>
                                 <li>
-                                    <a href="#projects">Projects</a>
+                                    <a
+                                        href="#projects"
+                                        className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                            activeId === 'projects' &&
+                                            'border-b-4 border-solid border-primary'
+                                        }`}
+                                    >
+                                        Projects
+                                    </a>
                                 </li>
                             </ul>
                         </div>
@@ -69,25 +154,81 @@ function Navbar() {
                             className="no-scrollbar dropdown-content menu rounded-box menu-compact mt-4 w-auto bg-base-100 p-2 shadow"
                         >
                             <li>
-                                <a href="#header">Home</a>
+                                <a
+                                    href="#header"
+                                    className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                        activeId === 'header' &&
+                                        'border-b-4 border-solid border-primary'
+                                    }`}
+                                >
+                                    Home
+                                </a>
                             </li>
                             <li>
-                                <a href="#about">About</a>
+                                <a
+                                    href="#about"
+                                    className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                        activeId === 'about' &&
+                                        'border-b-4 border-solid border-primary'
+                                    }`}
+                                >
+                                    About
+                                </a>
                             </li>
                             <li>
-                                <a href="#skills">Skills</a>
+                                <a
+                                    href="#skills"
+                                    className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                        activeId === 'skills' &&
+                                        'border-b-4 border-solid border-primary'
+                                    }`}
+                                >
+                                    Skills
+                                </a>
                             </li>
                             <li>
-                                <a href="#experiences">Experiences</a>
+                                <a
+                                    href="#experiences"
+                                    className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                        activeId === 'experiences' &&
+                                        'border-b-4 border-solid border-primary'
+                                    }`}
+                                >
+                                    Experiences
+                                </a>
                             </li>
                             <li>
-                                <a href="#education">Education</a>
+                                <a
+                                    href="#education"
+                                    className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                        activeId === 'education' &&
+                                        'border-b-4 border-solid border-primary'
+                                    }`}
+                                >
+                                    Education
+                                </a>
                             </li>
                             <li>
-                                <a href="#projects">Projects</a>
+                                <a
+                                    href="#projects"
+                                    className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                        activeId === 'projects' &&
+                                        'border-b-4 border-solid border-primary'
+                                    }`}
+                                >
+                                    Projects
+                                </a>
                             </li>
                             <li>
-                                <a href="#contact">Contact</a>
+                                <a
+                                    href="#contact"
+                                    className={`rounded transition-all duration-150 hover:border-b-4 hover:border-dotted hover:border-primary ${
+                                        activeId === 'contact' &&
+                                        'border-b-4 border-solid border-primary'
+                                    }`}
+                                >
+                                    Contact
+                                </a>
                             </li>
                         </ul>
                     </div>
